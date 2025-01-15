aee2f50f58f80f42b0bb6018cae0ad9514e43885
g2k4yh6p1-admin <g2k4yh6p1@gmail.com>
2025-01-14 20:32:27 -0500

    v

--- elevage.sql
+++ elevage.sql
@@ -1,22 +1,24 @@
 DROP
 DATABASE IF EXISTS elevage;
 CREATE
-DATABASE elevage;
+DATABASE elevage CHARACTER SET 'utf8';
 USE
 elevage;
-DROP
-DATABASE IF EXISTS Animal;
 CREATE TABLE Animal
 (
     id             SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    espace         VARCHAR(40) NOT NULL,
+    espece         VARCHAR(40) NOT NULL,
     sexe           CHAR(1),
     date_naissance DATETIME    NOT NULL,
     nom            VARCHAR(30),
-    commentaire    TEXT,
-    PRIMARY KEY (id) comment
+    commentaires   TEXT,
+    PRIMARY KEY (id)
 ) ENGINE=InnoDB;
-INSERT INTO Animal VALUES (1, 'chien ', 'M', '2017-04-05 13:43:00', 'Oli', 'Mordille beaucoup ');
-INSERT INTO Animal VALUES (2, 'chat', NULL , '2017-03-24 02:23:00', 'Luna ', NULL);
-INSERT INTO Animal (nom, commentaires, date_naissance, espece) VALUES ('Choupi ', 'Né sans oreille gauche ', '2010 -10 -03 16:44:00', 'chat ');
-INSERT INTO Animal (espece, date_naissance, commentaires, nom, sexe) VALUES ('tortue ', '2009 -06 -13 08:17:00', 'Carapace bizarre ', 'Bobosse ', 'F');
+INSERT INTO Animal
+VALUES (1, 'chien ', 'M', '2017-04-05 13:43:00', 'Oli', 'Mordille beaucoup ');
+INSERT INTO Animal
+VALUES (2, 'chat', NULL, '2017-03-24 02:23:00', 'Luna ', NULL);
+INSERT INTO Animal (espece, sexe, date_naissance, nom)
+VALUES ('chien ', 'F', '2008-12-06 05:18:00', 'Caroline'),
+       ('chat ', 'M', '2008-09-11 15:38:00', 'Bagherra'),
+       ('tortue ', NULL, '2010-08-23 05:18:00', NULL);
